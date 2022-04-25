4c0123fe1f54fc849f2c1b8caaf08bba70e54271
shigeo <shigeo@localhost>
2022-04-25 15:45:37 -0500

    vista

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import LoginPage from '@/views/LoginPage'
 
 Vue.use(VueRouter)
 
+/*
 const routes = [
   {
     path: '/',
@@ -16,7 +17,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import('../views/AboutView.vue')
+  }
+]
+*/
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginPage
   }
 ]
 
