1b386f4c55d661b4de8833a0f429c13dc19369a0
shigeo <shigeo@localhost.localdomain>
2022-04-25 14:20:00 -0500

    primer

---
+++ src/test/java/co/sn/taskagile/TaskagileApplicationTests.java
@@ -0,0 +1,13 @@
+package co.sn.taskagile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskagileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
